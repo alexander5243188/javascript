f4d3e4c4c2f46b69968d0491d58ed6b36d32923d
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-05-23 19:35:18 -0400

    concatenar strings con join, concat

---
+++ string.js
@@ -0,0 +1,16 @@
+const nombre = 'dani';
+const ocupacion = 'joda';
+
+const tiempoLibre = `Hola mi nombre es ${nombre} y mi tiempo libre lo dedico a la ${ocupacion}`
+
+console.log(tiempoLibre);
+
+const primeraParte ='Alegria';
+const segundaParte = 'de';
+const terceraParte = 'ENKI';
+const estiloDeVida = [primeraParte, segundaParte, terceraParte];
+
+console.log(estiloDeVida);
+console.log(estiloDeVida.join(' '));
+console.log("Mi vida cambio gracias a ".concat(primeraParte,' ', segundaParte, ' ',terceraParte));
+
