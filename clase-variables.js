822f46fc51e5ed24128f382402ddaa181b9c3f2e
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-05-23 17:58:14 -0400

    inmutabilidad y mutabilidad

--- clase-variables.js
+++ clase-variables.js
@@ -1,2 +1,45 @@
 let cajaDeAndy = "Woody";
 console.log(cajaDeAndy);
+
+// Tipos de datos 
+// Primitivos, Inmutabilidad, se pasan por valor
+
+let number = 26;
+number = number +10;
+console.log(number);
+
+
+let esVerdadero = true;
+esVerdadero = false;
+console.log(esVerdadero);
+
+
+// Complejos, Mutables se pasan por referencia
+
+let usuario ={
+    nombre: 'dani',
+    edad: 36
+}
+usuario.edad = 100;
+console.log(usuario);
+
+let frutas = ['manzana', 'pera'];
+frutas[0] = 'sandia';
+console.log(frutas);
+
+function cambiarNombre(objeto){
+    objeto.nombre = "angi",
+    objeto.edad = 0
+}
+
+let persona ={
+    nombre:'dani',
+    edad:33
+}
+
+cambiarNombre(persona);
+console.log(persona);
+
+
+    
+// Tipado dinamico
