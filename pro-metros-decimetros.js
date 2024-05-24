07dd5a2c79172994b839c47887878ebbe82fb48d
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-05-24 13:36:17 -0400

    metros a decimetros, centimetros,  milimetros

---
+++ pro-metros-decimetros.js
@@ -0,0 +1,8 @@
+let metros = 3.2;
+let decimetros = metros * 10;
+let centimetros = metros * 100;
+let milimetros = metros * 1000;
+console.log(`${metros} metro(s) son: `);
+console.log(`${decimetros} decimetros`);
+console.log(`${centimetros} centimetros`);
+console.log(`${milimetros} milimetros`);
