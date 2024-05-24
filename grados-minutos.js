e02eb5fe6f5764aa81aed7bfdffa1b75898c15da
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-05-24 13:10:19 -0400

    convertir grados a minutos y segundos

---
+++ grados-minutos.js
@@ -0,0 +1,10 @@
+// 1 grado tiene 60 minutos
+// 1 minuto tiene 60 segundos
+
+let grados = 33;
+let minutos = grados * 60;
+let segundos = minutos * 60;
+
+console.log(`${grados} grados son `);
+console.log(`${minutos} minutos` );
+console.log(`${segundos} segundos`);
