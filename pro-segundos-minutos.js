fb208e6673d14534df6152e194e238733001141e
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-05-24 13:29:38 -0400

    segundos a minutos

---
+++ pro-segundos-minutos.js
@@ -0,0 +1,7 @@
+let segundos = 33600;
+let minutos = segundos / 60;
+let horas = minutos / 60;
+
+console.log(`${segundos} segundos, son `);
+console.log(`${minutos} minutos`);
+console.log(`${horas.toFixed(1)} hora(s)`);
