09e729425f0c7e69adddb947aa43aec622a2bea6
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-05-24 10:57:34 -0400

    tipo de datos primitivos null, undefined, symbol

---
+++ null-undefined.js
@@ -0,0 +1,13 @@
+let dia = null;
+console.log(dia);
+console.log(typeof dia);
+
+let semana;
+console.log(semana);
+console.log(typeof semana);
+
+let uniqueID = Symbol('1');
+let uniqueIDUser = Symbol('1');
+
+console.log(uniqueID, uniqueIDUser);
+console.log(uniqueID === uniqueIDUser);
