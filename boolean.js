c1cb2921f06fe0c7d8adc7975d9a8e3fde80f01c
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-05-24 10:43:56 -0400

    conversion explisita e implicita

---
+++ boolean.js
@@ -0,0 +1,14 @@
+let isActive = true;
+let hasPermission = true;
+
+//conversion implicita
+let resultado = 5 < 3;
+console.log('5 < 3 = ', resultado);
+
+let nombre = 'dani';
+console.log(!!nombre);
+
+// conversion explicita
+let valor = 0;
+let valorNuevo = Boolean(valor);
+console.log(valorNuevo);
