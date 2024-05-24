1b8ebc876dfaf5787ee1b82c5c543aadae482fca
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-05-24 13:04:22 -0400

    dado el valor b, calcular las expresiones

---
+++ pro-valor-b.js
@@ -0,0 +1,10 @@
+let valorB = 33;
+let valorA = valorB + 10;
+let valorC = valorA + valorB;
+let valorD = valorC - valorA + 5;
+let valorE = valorA + valorB + valorC + valorD;
+
+console.log(`${valorB} + 10 = ${valorA}`);
+console.log(`${valorA} + ${valorB} = ${valorC}`);
+console.log(`${valorC} - ${valorA} + 5 = ${valorD}`);
+console.log(`${valorA} + ${valorB} + ${valorC} + ${valorD} = ${valorE}`);
