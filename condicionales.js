5e81b2562d939a75928465b5fd3b142842161dc9
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-05-24 20:07:27 -0400

    condicionales

---
+++ condicionales.js
@@ -0,0 +1,9 @@
+let numero = 10;
+
+if (numero > 0) {
+    console.log(`El ${numero} es positivo`);    
+} else if(numero  < 0){
+    console.log(`El ${numero} es negativo`);
+} else {
+    console.log(`El valor es ${numero}`);
+}
